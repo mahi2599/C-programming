d3738504c9be6b19d306e3695250f20a043c4dbf
mahi2599 <38681747+mahi2599@users.noreply.github.com>
2018-04-24 12:24:45 +0530

    Create positive.c

---
+++ codekata/positive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int n;
+  scanf("%d",&n);
+  if(n=>1&&N<=100000)
+     if(n.=1)
+           printf("Positive")
+     else(n==0)
+           printf("Zero");
+     else
+           printf("Negative");
+  else
+           printf("Invalid");
+  }         
