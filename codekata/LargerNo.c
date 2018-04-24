b7c598d988765b5a37913fff6c4fc3be6d281713
mahi2599 <38681747+mahi2599@users.noreply.github.com>
2018-04-24 14:48:30 +0530

    Update LargerNo.c

--- codekata/LargerNo.c
+++ codekata/LargerNo.c
@@ -7,13 +7,13 @@ int main()
     scanf("%d %d %d", &n1, &n2, &n3);
 
     if( n1>=n2 && n1>=n3 )
-        printf("%d is the largest number.", n1);
+        printf("%d", n1);
 
     if( n2>=n1 && n2>=n3 )
-        printf("%d is the largest number.", n2);
+        printf("%d", n2);
 
     if( n3>=n1 && n3>=n2 )
-        printf("%d is the largest number.", n3);
+        printf("%d", n3);
 
     return 0;
 }
